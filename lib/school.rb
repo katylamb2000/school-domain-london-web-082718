76ed501f1dba8831fc29e86b648bb72c63709ad1
katy@karstaway.com <katy@karstaway.com>
2018-09-02 13:41:19 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
-# code here!
+
+class School
+  
+  
+  def initialize(name)
+    @name = name
+    @roster
+
+ end
+  
+  def roster
+    @roster = {}
+  end
+  
+  def add_student (student, grade)
+    @roster["new"] = []
+    @roster["new"] << "student"
+    
+    # roster[grade] ||= []
+    # roster[grade] << student_name
+
+end
+
+# def add_student (student, grade)
+  
+#   roster = [grade => student]
+
+  
+ 
+end
